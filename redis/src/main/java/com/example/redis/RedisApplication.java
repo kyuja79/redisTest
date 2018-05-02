3b9e58e2e14b6707df0cc23ec2b50cf8393b0bd5
kyuja79 <kyuja79@192.168.0.103>
2018-05-03 01:51:24 +0900

    comments add

--- redis/src/main/java/com/example/redis/RedisApplication.java
+++ redis/src/main/java/com/example/redis/RedisApplication.java
@@ -11,6 +11,12 @@ import org.springframework.data.geo.Point;
 import org.springframework.data.redis.core.GeoOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 
+
+/**
+ * test redis geolocation
+ * @author kyuja79
+ *
+ */
 @SpringBootApplication
 public class RedisApplication {
 	
@@ -29,11 +35,25 @@ public class RedisApplication {
 			geo.add("Sicily", new Point(15.087269, 37.502669), "Catania");
 			geo.add("Sicily", new Point(13.583333, 37.316667), "Palermo");
 			
-			Circle circle = new Circle(new Point(13.583333, 37.316667),
+			Circle circle_100 = new Circle(new Point(13, 37),
 					new Distance(100, org.springframework.data.redis.connection.RedisGeoCommands.DistanceUnit.KILOMETERS));
 			
-			GeoResults<org.springframework.data.redis.connection.RedisGeoCommands.GeoLocation<String>> geoResults = geo.radius("Sicily", circle);
-			geoResults
+
+			Circle circle_200 = new Circle(new Point(13, 37),
+					new Distance(200, org.springframework.data.redis.connection.RedisGeoCommands.DistanceUnit.KILOMETERS));
+			
+			GeoResults<org.springframework.data.redis.connection.RedisGeoCommands.GeoLocation<String>> geoResults_100 = geo.radius("Sicily", circle_100);
+			System.out.println("geoResults_100 : ");
+			/**
+			 * find 100 km geo point
+			 */
+			geoResults_100
+				.getContent()
+				.forEach(c -> System.out.println(c.toString()));
+			
+			GeoResults<org.springframework.data.redis.connection.RedisGeoCommands.GeoLocation<String>> geoResults_200 = geo.radius("Sicily", circle_200);
+			System.out.println("geoResults_200 : ");
+			geoResults_200
 				.getContent()
 				.forEach(c -> System.out.println(c.toString()));
 			
